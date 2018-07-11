948897abc7d10d37baa9882b3cf70709199803b5
dzy <daizy15@mails.tsinghua.edu.cn>
2018-07-11 17:33:50 +0800

    Add register file module.
    Tests can be run with `sbt 'test:runMain gcd.tester -td test_run_dir'`

--- src/test/scala/gcd/MyGCDTest.scala
+++ src/test/scala/gcd/MyGCDTest.scala
@@ -3,7 +3,8 @@ package gcd
 import chisel3._
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
-class MyGCDTest(gcd: GCD ) extends PeekPokeTester(gcd) {
+
+class MyGCDTest(gcd: GCD) extends PeekPokeTester(gcd) {
   val a = 6
   val b = 9
 
@@ -17,11 +18,54 @@ class MyGCDTest(gcd: GCD ) extends PeekPokeTester(gcd) {
 
   expect(gcd.io.outputGCD, 3)
   expect(gcd.io.outputValid, 1)
-  
 }
+
+
+class MyRegFileTest(rf: RegFile) extends PeekPokeTester(rf) {
+  poke(rf.io.raddr1, 0)
+  poke(rf.io.raddr2, 0)
+  poke(rf.io.waddr, 0)
+  step(1)
+  expect(rf.io.rdata1, 0)
+  expect(rf.io.rdata2, 0)
+
+  poke(rf.io.waddr, 1)
+  poke(rf.io.wdata, 11111)
+  step(1)
+
+  poke(rf.io.waddr, 2)
+  poke(rf.io.wdata, 22222)
+  step(1)
+
+  poke(rf.io.waddr, 3)
+  poke(rf.io.wdata, 33333)
+  step(1)
+
+  poke(rf.io.raddr1, 1)
+  poke(rf.io.raddr2, 2)
+  expect(rf.io.rdata1, 11111)
+  expect(rf.io.rdata2, 22222)
+  poke(rf.io.raddr1, 3)
+  poke(rf.io.raddr2, 1)
+  expect(rf.io.rdata1, 33333)
+  expect(rf.io.rdata2, 11111)
+
+  poke(rf.io.waddr, 31)
+  poke(rf.io.wdata, 12345)
+  step(1)
+  poke(rf.io.raddr1, 31)
+  expect(rf.io.rdata1, 12345)
+}
+
 
 object tester {
   def main(args: Array[String]): Unit = {
+    println("MyRegFileTest")
+    assert(
+      iotesters.Driver.execute(args, () => new RegFile()) {
+        c => new MyRegFileTest(c)
+      })
+    println("MyGCDTest")
     assert(
       iotesters.Driver.execute(args, () => new GCD()) {
         c => new MyGCDTest(c)
