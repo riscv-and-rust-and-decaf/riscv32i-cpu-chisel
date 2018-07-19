8118d5277bc5fe8a378ccde8823d84dce1c79a57
WangRunji <wangrunji0408@163.com>
2018-07-19 08:50:20 +0800

    Generate .v at ./out dir

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,17 @@
 import chisel3._
+import java.io.{File, FileWriter}
 
 object Gen {
   def main(args: Array[String]) : Unit = {
-    chisel3.Driver.execute(args, () => new Core())
+    val dirname = if(args.length == 0) "out" else args(0)
+    val dir = new File(dirname); dir.mkdirs
+    val chirrtl = firrtl.Parser.parse(chisel3.Driver.emit(() => new Core))
+    val writer = new FileWriter(new File(dir, s"${chirrtl.main}.fir"))
+    writer write chirrtl.serialize
+    writer.close
+
+    val verilog = new FileWriter(new File(dir, s"${chirrtl.main}.v"))
+    verilog write (new firrtl.VerilogCompiler).compileAndEmit(firrtl.CircuitState(chirrtl, firrtl.ChirrtlForm)).getEmittedCircuit.value
+    verilog.close
   }
 }
