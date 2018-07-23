1445082d290d2bc702daf048c87a4f594c25efa6
WangRunji <wangrunji0408@163.com>
2018-07-23 19:57:54 +0800

    Top module for connecting to Verilog

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,25 @@
-import core_.Core
+import chisel3._
+import core_._
+import devices.IOManager
+
+class ChiselTop extends Module {
+  val io = IO(new Bundle {
+    val ram  = new RAMOp
+    val flash = new RAMOp
+    val serial = new RAMOp
+  })
+
+  val core    = Module(new Core())
+  val ioCtrl  = Module(new IOManager())
+
+  core.io.dev <> ioCtrl.io.core
+  io.ram      <> ioCtrl.io.ram
+  io.flash    <> ioCtrl.io.flash
+  io.serial   <> ioCtrl.io.serial
+}
 
 object Gen {
   def main(args: Array[String]) : Unit = {
-    chisel3.Driver.execute(args, () => new Core())
+    chisel3.Driver.execute(args, () => new ChiselTop())
   }
 }
