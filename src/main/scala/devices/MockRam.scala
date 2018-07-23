66ce0282e15be20d040875e64734cffcb57766c1
WangRunji <wangrunji0408@163.com>
2018-07-24 00:33:14 +0800

    [BREAK CHANGE] MockRam/Serial now lock data at rising edge. All tests failed.

--- src/main/scala/devices/MockRam.scala
+++ src/main/scala/devices/MockRam.scala
@@ -10,47 +10,53 @@ class MockRam() extends Module {
 
   private val mem = Mem(0x800000, UInt(8.W))
 
-  io.ok := io.mode =/= RAMMode.NOP
-  val data = Cat((0 until 4).reverse.map(i => mem(io.addr + i.U)))
+  val mode = RegNext(io.mode, init=0.U)
+  val addr = RegNext(io.addr, init=0.U)
 
+  io.ok := mode =/= RAMMode.NOP
+  val data = Cat((0 until 4).reverse.map(i => mem(addr + i.U)))
+
+  // Mem write happens at rising edge.
+  // So must not use Reg.
   switch(io.mode) {
     is(RAMMode.SW) {
       for (i <- 0 until 4)
         mem(io.addr + i.U) := io.wdata(i * 8 + 7, i * 8)
-      printf("[RAM] SW: [%x]=%x\n", io.addr, io.wdata)
+      printf("[RAM] SW: [%x]=%x\n", addr, io.wdata)
     }
     is(RAMMode.SH) {
       mem(io.addr + 1.U) := io.wdata(15, 8)
       mem(io.addr) := io.wdata(7, 0)
-      printf("[RAM] SH: [%x]=%x\n", io.addr, io.wdata)
+      printf("[RAM] SH: [%x]=%x\n", addr, io.wdata)
     }
     is(RAMMode.SB) {
       mem(io.addr) := io.wdata(7, 0)
-      printf("[RAM] SB: [%x]=%x\n", io.addr, io.wdata)
+      printf("[RAM] SB: [%x]=%x\n", addr, io.wdata)
     }
   }
 
+  // To read at rising edge, have to use Reg.
   io.rdata := 0.U
-  switch(io.mode) {
+  switch(mode) {
     is(RAMMode.LW) {
       io.rdata := data
-      printf("[RAM] LW: [%x]->%x\n", io.addr, io.rdata)
+      printf("[RAM] LW: [%x]->%x\n", addr, io.rdata)
     }
     is(RAMMode.LH) {
       io.rdata := Cat(Mux(data(15), 0xff.U, 0.U), data(15, 0))
-      printf("[RAM] LH: [%x]->%x\n", io.addr, io.rdata)
+      printf("[RAM] LH: [%x]->%x\n", addr, io.rdata)
     }
     is(RAMMode.LB) {
       io.rdata := Cat(Mux(data(7), 0xfff.U, 0.U), data(7, 0))
-      printf("[RAM] LB: [%x]->%x\n", io.addr, io.rdata)
+      printf("[RAM] LB: [%x]->%x\n", addr, io.rdata)
     }
     is(RAMMode.LHU) {
       io.rdata := data(15, 0).zext.asUInt
-      printf("[RAM] LHU: [%x]->%x\n", io.addr, io.rdata)
+      printf("[RAM] LHU: [%x]->%x\n", addr, io.rdata)
     }
     is(RAMMode.LBU) {
       io.rdata := data(7, 0).zext.asUInt
-      printf("[RAM] LBU: [%x]->%x\n", io.addr, io.rdata)
+      printf("[RAM] LBU: [%x]->%x\n", addr, io.rdata)
     }
   }
 }
--- src/main/scala/devices/MockSerial.scala
+++ src/main/scala/devices/MockSerial.scala
@@ -15,14 +15,18 @@ class MockSerial extends Module {
   val canWrite = true.B
   val status = (canWrite << 5.U) | canRead
 
+  val mode = RegNext(io.mode, init=0.U)
+  val addr = RegNext(io.addr, init=0.U)
+  val wdata = io.wdata
+
   val rdata = Wire(UInt(8.W))
   rdata := 0.U
   // test
-  when(io.mode === RAMMode.LBU && io.addr(2,0) === 5.U) {
+  when(mode === RAMMode.LBU && addr(2,0) === 5.U) {
     rdata := status
   }
   // read
-  when(RAMMode.isRead(io.mode) && io.addr(2,0) === 0.U) {
+  when(RAMMode.isRead(mode) && addr(2,0) === 0.U) {
     when(canRead) {
       rdata := inputs(next_input_num)
       next_input_num := next_input_num + 1.U
@@ -30,10 +34,10 @@ class MockSerial extends Module {
     printf("[Serial] Read: (%d/%d) 0x%x, '%c'\n", next_input_num, inputs_raw.length.U, rdata, rdata)
   }
   // write
-  when(io.mode === RAMMode.SB && io.addr(2,0) === 0.U) {
-    printf("[Serial] Write: 0x%x, '%c'\n", io.wdata(7,0), io.wdata(7,0))
+  when(mode === RAMMode.SB && addr(2,0) === 0.U) {
+    printf("[Serial] Write: 0x%x, '%c'\n", wdata(7,0), wdata(7,0))
   }
 
-  io.rdata := Mux(rdata(7) && io.mode === RAMMode.LB, 0xffffff.U(24.W), 0.U(24.W)) ## rdata
-  io.ok := io.mode =/= RAMMode.NOP
+  io.rdata := Mux(rdata(7) && mode === RAMMode.LB, 0xffffff.U(24.W), 0.U(24.W)) ## rdata
+  io.ok := mode =/= RAMMode.NOP
 }
