46ec9fe248a1982f155675982423a817246a25a7
WangRunji <wangrunji0408@163.com>
2018-07-25 14:42:17 +0800

    Fix IF & MEM test

--- src/test/scala/core_/IFTest.scala
+++ src/test/scala/core_/IFTest.scala
@@ -11,13 +11,15 @@ class IFTest(t: IF) extends PeekPokeTester(t) {
     poke(t.io.mmu.ok, 1)
   }
 
+  val PC_BASE = 0x80000000L
+
   // sequential if
   reset_poke_init()
   for (i <- 0 until 7) {
     val some_inst = i*0x11110000 + (i+1)*0x00001111
     poke(t.io.mmu.rdata, some_inst)
-    expect(t.io.id.pc, i*4)
-    expect(t.io.mmu.addr, i*4)
+    expect(t.io.id.pc, PC_BASE + i*4)
+    expect(t.io.mmu.addr, PC_BASE + i*4)
     expect(t.io.mmu.mode, RAMMode.LW)
     expect(t.io.id.inst, some_inst)
     step(1)
@@ -28,7 +30,7 @@ class IFTest(t: IF) extends PeekPokeTester(t) {
   poke(t.io.mmu.ok, 0)
   step(1)
   expect(t.io.id.pc, 0)
-  expect(t.io.mmu.addr, 0)
+  expect(t.io.mmu.addr, PC_BASE)
   expect(t.io.mmu.mode, RAMMode.LW)
   step(1)
   expect(t.io.id.pc, 0)
@@ -40,24 +42,24 @@ class IFTest(t: IF) extends PeekPokeTester(t) {
   expect(t.io.id.pc, 0)
   poke(t.io.id.ready, 1)
   step(1)
-  expect(t.io.id.pc, 4)
+  expect(t.io.id.pc, PC_BASE + 4)
 
   // Branch (inst is ignored)
   reset_poke_init()
   //   Only branch
   poke(t.io.id.branch.valid, 1)
-  poke(t.io.id.branch.bits, 40)
+  poke(t.io.id.branch.bits, PC_BASE + 40)
   step(1)
-  expect(t.io.id.pc, 40)
+  expect(t.io.id.pc, PC_BASE + 40)
   //   Branch with stall
   poke(t.io.id.branch.valid, 1)
-  poke(t.io.id.branch.bits, 80)
+  poke(t.io.id.branch.bits, PC_BASE + 80)
   poke(t.io.mmu.ok, 0)
   step(1)
   expect(t.io.id.pc, 0)
   poke(t.io.mmu.ok, 1)
   step(1)
-  expect(t.io.id.pc, 80)
+  expect(t.io.id.pc, PC_BASE + 80)
 }
 
 
--- src/test/scala/core_/MEMTest.scala
+++ src/test/scala/core_/MEMTest.scala
@@ -7,14 +7,18 @@ import Const._
 import OptCode._
 
 class MEMTest(mem: MEM) extends PeekPokeTester(mem) {
+  // No IO
   poke(mem.io.ex.ramOp.mode, RAMMode.NOP)
   poke(mem.io.ex.wrRegOp.addr, 10)
   poke(mem.io.ex.wrRegOp.data, 44)
+  poke(mem.io.mmu.ok, 0)
   step(1)
   expect(mem.io.wrRegOp.addr, 10)
   expect(mem.io.wrRegOp.data, 44)
   expect(mem.io.mmu.mode, RAMMode.NOP)
+  expect(mem.io.ex.ready, true)
 
+  // Write on 1 cycle
   poke(mem.io.ex.ramOp.mode, RAMMode.SW)
   poke(mem.io.ex.ramOp.addr, 44)
   poke(mem.io.ex.ramOp.wdata, 9901)
@@ -22,13 +26,29 @@ class MEMTest(mem: MEM) extends PeekPokeTester(mem) {
   expect(mem.io.mmu.mode, RAMMode.SW)
   expect(mem.io.mmu.addr, 44)
   expect(mem.io.mmu.wdata, 9901)
+  poke(mem.io.mmu.ok, 1)
+  expect(mem.io.ex.ready, true)
 
+  // Read on 2 cycle
   poke(mem.io.ex.ramOp.mode, RAMMode.LW)
-  poke(mem.io.mmu.rdata, 4321)
+  poke(mem.io.ex.ramOp.addr, 44)
+  step(1)
+  poke(mem.io.mmu.ok, 0)
+  expect(mem.io.wrRegOp.addr, 0)
+  expect(mem.io.ex.ready, false)
+
+  //   Should keep last input when stall
+  poke(mem.io.ex.ramOp.mode, 0)
+  poke(mem.io.ex.ramOp.addr, 0)
   step(1)
   expect(mem.io.mmu.mode, RAMMode.LW)
+  expect(mem.io.mmu.addr, 44)
+
+  poke(mem.io.mmu.ok, 1)
+  poke(mem.io.mmu.rdata, 4321)
   expect(mem.io.wrRegOp.addr, 10)
   expect(mem.io.wrRegOp.data, 4321)
+  expect(mem.io.ex.ready, true)
 }
 
 class MEMTester extends ChiselFlatSpec {
