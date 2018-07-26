fe5314d2781fa25dd71ece19c276f175889a6bc8
dzy <daizy15@mails.tsinghua.edu.cn>
2018-07-26 17:20:36 +0800

    Fix out of memory problems. Move CSR tests out from Core tests.

---
+++ src/test/scala/core_/CSRTest.scala
@@ -0,0 +1,39 @@
+package core_
+
+import chisel3._
+import chisel3.iotesters._
+import devices._
+
+class CSRInstTest(c: CoreTestModule, fname: String) extends CoreTest(c, fname) {
+  step(17)
+  expect(c.d.reg(2), 13)
+  expect(c.d.reg(3), 11)
+  expect(c.d.reg(4), 8)
+  expect(c.d.reg(5), 10)
+}
+
+class ECALLTest(c: CoreTestModule, fname: String) extends CoreTest(c, fname) {
+  step(100)
+  expect(c.d.reg(1), 13)
+  expect(c.d.reg(8), 8)
+  expect(c.d.reg(20), 11)
+  expect(c.d.reg(30), 30)
+}
+
+class CoreCSRTester extends ChiselFlatSpec {
+  val args = Array[String]("-fiwv")
+  "Core simple csr test" should "pass test" in {
+    iotesters.Driver.execute(args, () => new CoreTestModule()) {
+      c => new CSRInstTest(c, "test_asm/test_csr.bin")
+    } should be (true)
+  }
+}
+
+class CoreECALLTester extends ChiselFlatSpec {
+  val args = Array[String]("-fiwv")
+  "Core ecall test" should "pass test" in {
+    iotesters.Driver.execute(args, () => new CoreTestModule()) {
+      c => new ECALLTest(c, "test_asm/test_ecall.bin")
+    } should be (true)
+  }
+}
