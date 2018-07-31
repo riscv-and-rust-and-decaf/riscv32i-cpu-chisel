7605990a561363afe33211800d62a354e47d2524
WangRunji <wangrunji0408@163.com>
2018-07-31 16:01:45 +0800

    Fix misa. Pass test `rv32mi-p-mcsr`.

--- src/main/scala/core_/CSR.scala
+++ src/main/scala/core_/CSR.scala
@@ -97,21 +97,15 @@ class CSR extends Module {
     val UIE = Bool()
   }
 
-
-  // read-only m info
-  val mvendorid = 2333.U(32.W)
-  val marchid   = "h8fffffff".U(32.W)
-  val mimpid    = 2333.U(32.W)
-  val mhartid   = 0.U(32.W)
-
   val mstatus = RegInit(0.U.asTypeOf(new MStatus))
 
   // Read CSR from ID
   io.id.rdata := MuxLookup(io.id.addr, csr(io.id.addr), Seq(
-    ADDR.mvendorid -> mvendorid,
-    ADDR.marchid -> marchid,
-    ADDR.mimpid -> mimpid,
-    ADDR.mhartid -> mhartid,
+    ADDR.mvendorid -> 2333.U(32.W),
+    ADDR.marchid -> "h8fffffff".U(32.W),
+    ADDR.mimpid -> 2333.U(32.W),
+    ADDR.mhartid -> 0.U(32.W),
+    ADDR.misa -> (1 << 30 | 1 << ('I' - 'A')).U(32.W),
     ADDR.mstatus -> mstatus.asUInt
   ))
   io.id.prv := prv
