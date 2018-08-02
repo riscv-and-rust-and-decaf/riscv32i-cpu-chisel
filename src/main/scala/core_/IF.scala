848d21527236622142caee67d34f8661bdacc18c
WangRunji <wangrunji0408@163.com>
2018-08-02 18:22:35 +0800

    Avoid repeated IO in IF

--- src/main/scala/core_/IF.scala
+++ src/main/scala/core_/IF.scala
@@ -10,30 +10,41 @@ class IF extends Module {
     val id = new IF_ID
   })
 
-  val stall = !io.mmu.ok || !io.id.ready
+  val pc         = RegInit(Const.PC_INIT)
+  val log_branch = RegInit(0.U.asTypeOf(Valid(UInt(32.W))))
+  val log_inst   = RegInit(0.U.asTypeOf(Valid(UInt(32.W))))
 
-  val pc      = RegInit(Const.PC_INIT)
-  val branch  = RegInit(0.U.asTypeOf(Valid(UInt(32.W))))
+  val mmu_inst = Wire(Valid(UInt(32.W)))
+  mmu_inst.valid := io.mmu.ok
+  mmu_inst.bits := io.mmu.rdata
 
-  // Log branch
+  val branch = Mux(io.id.branch.valid, io.id.branch, log_branch)
+  val inst   = Mux(log_inst.valid, log_inst, mmu_inst)
+
+  // Log branch & inst
   when(io.id.branch.valid) {
-    branch := io.id.branch
+    log_branch := io.id.branch
   }
+  when(!log_inst.valid && io.mmu.ok) {
+    log_inst.valid := true.B
+    log_inst.bits := io.mmu.rdata
+  }
+
+  val stall = !inst.valid || !io.id.ready
 
   // Change status only when mmu.ok
   when(!stall) {
     pc := PriorityMux(Seq(
-      (io.id.branch.valid,  io.id.branch.bits),
-      (branch.valid,        branch.bits),
-      (true.B,              pc + 4.U)))
-    branch := 0.U.asTypeOf(Valid(UInt(32.W))) // Clear branch log
+      (branch.valid, branch.bits),
+      (true.B,       pc + 4.U)))
+    log_branch := 0.U.asTypeOf(Valid(UInt(32.W))) // Clear branch log
+    log_inst   := 0.U.asTypeOf(Valid(UInt(32.W))) // Clear inst log
   }
 
   // instruction fetch
   io.mmu.addr  := pc; // fetch current instruction
-  io.mmu.mode  := Mux(reset.toBool, RAMMode.NOP, RAMMode.LW)
+  io.mmu.mode  := Mux(log_inst.valid, RAMMode.NOP, RAMMode.LW)
   io.mmu.wdata := 0.U
-
 
   // Feed to ID: valid only when no stall && no branch
 
@@ -41,8 +52,8 @@ class IF extends Module {
   io.id.inst  := Const.NOP_INST
   io.id.excep := 0.U.asTypeOf(new Exception)
 
-  when(!(stall || branch.valid || io.id.branch.valid)) {
-    io.id.inst := io.mmu.rdata
+  when(!(stall || branch.valid)) {
+    io.id.inst := inst.bits
     io.id.excep.pc := pc
     io.id.excep.valid_inst := true.B
     when(pc(1,0).orR) {
