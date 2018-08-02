455203dfdbeec1790cd5e5b94d531b22fb6f38d1
dzy <daizy15@mails.tsinghua.edu.cn>
2018-08-02 22:02:52 +0800

    Fix IDTest. Pokes in io.iff.pc to avoid misaligned branches.

--- src/test/scala/core_/IDTest.scala
+++ src/test/scala/core_/IDTest.scala
@@ -6,6 +6,7 @@ import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 class IDTest(t: ID) extends PeekPokeTester(t) {
   reset(10)
+  poke(t.io.iff.pc, "h_8000_0000".U(32.W))
   poke(t.io.iff.excep.valid, false.B)
   poke(t.io.flush, false.B)
   poke(t.io.ex.ready, true)
@@ -15,12 +16,13 @@ class IDTest(t: ID) extends PeekPokeTester(t) {
   poke(t.io.reg.read1.data,7)
   poke(t.io.reg.read2.data,8)
   step(1)
-
+  expect(t.io.iff.branch.valid, false.B)
   expect(t.io.ex.aluOp.rd1, 7)
   expect(t.io.ex.aluOp.rd2, "h_ffff_fffe".U)
   expect(t.io.ex.aluOp.opt, OptCode.SLT)
 
-  poke(t.io.iff.inst, "h_fea0_9ce3".U)
+  poke(t.io.iff.pc, "h_8000_0010".U(32.W))
+  poke(t.io.iff.inst, "h_fea0_9ce3".U) // bne x1 x10 -8
   // 1111_1110_1010_0000_1001_1100_1110_0011
   poke(t.io.reg.read1.data, 1)
   poke(t.io.reg.read2.data, 10)
@@ -30,7 +32,7 @@ class IDTest(t: ID) extends PeekPokeTester(t) {
   expect(t.d.bt, "b0_1010".U)
   expect(t.d.l, true.B)
   expect(t.io.iff.branch.valid, true.B)
-
+  expect(t.io.iff.branch.bits, "h_8000_0008".U(32.W))
 }
 
 class IDTester extends ChiselFlatSpec {
