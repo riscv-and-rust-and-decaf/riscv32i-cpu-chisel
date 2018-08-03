e826d6c15eb8df538903014137e426bd2cb0187f
WangRunji <wangrunji0408@163.com>
2018-08-03 23:28:51 +0800

    Add verilator to travis

---
+++ tools/install-verilator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+mkdir $ROOT/tmp
+cd $ROOT/tmp
+
+if [ ! -e "$ROOT/tmp/bin/verilator" ]; then
+    echo "Installing Verilator"
+    wget https://www.veripool.org/ftp/verilator-3.900.tgz
+    tar xzf verilator*.t*gz && cd verilator-*
+    autoconf && ./configure --prefix="$ROOT/tmp" && make -j2 && make test && make install
+else
+    echo "Using Verilator from cached directory."
+fi
